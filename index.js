6cd0a27f5a7cdfc22be56424e7a5fc22cc1def31
Christian Yonge <christianyonge93@gmail.com>
2019-12-12 20:44:48 -0600

    added the pdf rendering code block at line 48 and 95

--- index.js
+++ index.js
@@ -2,10 +2,7 @@ const fs = require("fs");
 const inquirer = require("inquirer");
 const util = require("util");
 const axios = require("axios");
-const convertFactory = require('electron-html-to');
-
-
-const writeFileAsync = util.promisify(fs.writeFile);
+const pdf = require('html-pdf');
 
 
 
@@ -35,83 +32,83 @@ inquirer
         // const promise3 = axios.get(URL3);
 
         Promise.all([promise1, promise2])
-    .then(function (res) {
-        const pdfInfo = {
-            "profile" : res[0].data.html_url,
-            "profilePic" : res[0].data.avatar_url,
+            .then(function (res) {
+                const pdfInfo = {
+                    "profile": res[0].data.name,
+                    "profilePic": res[0].data.avatar_url,
 
-            
-        };
-        let stargazers = 0;
-            res[1].data.forEach(y => stargazers += y.stargazers_count);
-        console.log(stargazers)
 
-        console.log(res);
-        console.log(pdfInfo);
+                };
+                let stargazers = 0;
+                res[1].data.forEach(y => stargazers += y.stargazers_count);
 
-        var conversion = convertFactory({
-            converterPath: convertFactory.converters.PDF
-          });
-        
-
-          conversion({ html: 
-            `
-            <!DOCTYPE html>
-            <html lang="en">
-            <head>
-                <meta charset="UTF-8">
-                <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                <meta http-equiv="X-UA-Compatible" content="ie=edge">
-                <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
-                    integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-                <title>Document</title>
-            </head>
-            
-            <body>
-                <div class="jumbotron">
-                    <h1 class="display-4">Hi, my name is ${username}!</h1>
-                    <p class="lead">I am from ${username}</p>
-                    <hr class="my-4">
+                function makeHtml(res){
+                    return `
+                    <!DOCTYPE html>
+                    <html lang="en">
+                    <head>
+                        <meta charset="UTF-8">
+                        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+                        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+                            integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+                        <title>Document</title>
+                    </head>
                     
-            
-                    <ul class="list-group">
-                        <li class="list-group-item">Github Username: ${username}</li>
-                        <li class="list-group-item">Linkedin Link: ${res.profile}</li>
-                        <li class="list-group-item">Ima slam some ${res.profilePic}</li>
-                    </ul>
-                    <hr class="my-4">
-                    <!-- This line below would link to contact page -->
-                    <a class="btn btn-primary btn-lg" href="#" role="button">Contact Me</a>
-                </div>
-                <div>
-                <h2>Github Stars:</h2>
-                <h2>${stargazers}</h2>
-            </div>
-            </body>
-            
-            </html>
-            `
-        }, function(err, PDF) {
-                if (err) {
-                  return console.error(err);
+                    <body>
+                        <div class="jumbotron">
+                            <h1 class="display-4">Hi, my name is ${username}!</h1>
+                            <p class="lead">I am from ${res.location}</p>
+                            <hr class="my-4">
+                            
+                    
+                            <ul class="list-group">
+                                <li class="list-group-item">Github Username: ${res.username}</li>
+                                <li class="list-group-item">Linkedin Link: ${res.profile}</li>
+                                <li class="list-group-item">Ima slam some ${res.profilePic}</li>
+                            </ul>
+                            <hr class="my-4">
+                            <!-- This line below would link to contact page -->
+                            <a class="btn btn-primary btn-lg" href="#" role="button">Contact Me</a>
+                        </div>
+                        <div>
+                        <h2>Github Stars:</h2>
+                        <h2>${stargazers}</h2>
+                    </div>
+                    </body>
+                    
+                    </html>)
+                    `;
+                     
                 }
-               
-                console.log(PDF.numberOfPages);
-                console.log(PDF.logs);
-                PDF.stream.pipe(fs.createWriteStream('.pdf'));
-              });;
-             
-       
-        
+                
+                console.log(stargazers)
 
-        })
-    .catch(function(err) {
-        console.log(err);
+                console.log(res);
+                console.log(pdfInfo);
 
-        
+                
+                var options = { format: 'Letter' };
+
+
+                // get rid of 'options' here +==================================================
+                pdf.create(makeHtml(res), options).toFile('Profile.pdf', function (err, res) {
+                    if (err) return console.log(err);
+                    console.log(res); // { filename: '/app/businesscard.pdf' }
+                });
+
+
+
+
+
+            })
+            .catch(function (err) {
+                console.log(err);
+
+
 
             });
-        });
+    });
 
     // });
 
